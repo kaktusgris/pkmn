f97e9e5105ab45c742a024e3e705c082775cc0c1
Marton Skjæveland <martonsk@stud.ntnu.no>
2021-05-20 22:02:10 +0200

    Fjernet .idea/

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Home = () => {
-    return <div>Home</div>
+    return (
+        <div>
+            <h1>Diverse pokemonverktøy</h1>
+            <p>Wizard er for "wizard challenge"</p>
+            <p>Magic Babies inneholder en liste av challenges for bruk i Magic Babies eller lignende</p>
+        </div>
+    )
 }
 
 export default Home;
